1b2356e46d32defe7ab2266a87422b498ace0a41
Hugo Herter <okso@okso.local>
2012-12-04 12:26:31 +0100

    Work on data loading

--- gaproject/data.py
+++ gaproject/data.py
@@ -1,5 +1,15 @@
 
+import os.path
 
-def get_data(path):
-    "Returns the content of a data file from it's name."
-    raise NotImplementedError
+
+class Box(object):
+    "Access to load data from a data directory."
+
+    def __init__(self, root):
+        "Initialize with the path of the data directory."
+        self.root = os.path.abspath(root)
+
+    def get_data(self, path):
+        "Returns the content of a data file from it's name."
+        full_path = os.path.join(self.root, path)
+        return open(full_path)
--- gaproject/main.py
+++ gaproject/main.py
@@ -4,12 +4,13 @@
 This is the main file of the project, used to setup and launch the computation.
 '''
 
-from gaproject.data import get_data
+from gaproject.data import Box
 
 
 def main():
     print 'Starting...'
-    #data = get_data('data')
+    box = Box('data/TSPBenchmark')
+    data = box.get('belgiumtour.tsp')
 
 if __name__ == '__main__':
     main()
