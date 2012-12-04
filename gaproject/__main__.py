f07322472343d4b1b6401aaf7e0da081ed7de164
Hugo Herter <okso@okso.local>
2012-12-04 15:01:54 +0100

    Created MyDeap class to use DEAP

--- gaproject/__main__.py
+++ gaproject/__main__.py
@@ -5,16 +5,33 @@ This is the main file of the project, used to setup and launch the computation.
 '''
 
 from gaproject.data import Box
-from gaproject.mydeap import creator
+from gaproject.mydeap import MyDeap
 
 
 def main():
     print 'Starting...'
     box = Box('data/TSPBenchmark')
-    data = box.get('belgiumtour.tsp')
+    # data = box.get('belgiumtour.tsp')
+    data = box.get('xqf131.tsp')
     # print data.positions
     # print data.dist_matrix()
-    print len(data)
+    # print len(data)
+
+    distance_map = data.dist_matrix()
+
+    def evalTSP(individual):
+        distance = distance_map[individual[-1]][individual[0]]
+        for gene1, gene2 in zip(individual[0:-1], individual[1:]):
+            distance += distance_map[gene1][gene2]
+        return distance,
+
+    mydeap = MyDeap()
+    print mydeap.creator()
+    print mydeap.toolbox(len(data), evalTSP)
+
+    toolbox = mydeap.toolbox(len(data), evalTSP)
+
+    pop, stats, hof = mydeap.run(toolbox)
 
 
 if __name__ == '__main__':
--- gaproject/mydeap.py
+++ gaproject/mydeap.py
@@ -3,11 +3,65 @@ Generic setup for DEAP in this project.
 '''
 
 import array
+import random
 
 from deap import algorithms
 from deap import base
 from deap import creator
 from deap import tools
 
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-creator.create("Individual", array.array, typecode='i', fitness=creator.FitnessMin)
+
+class MyDeap(object):
+    "Class for generic setup of DEAP in our problem."
+
+    def creator(self):
+        "Setting up the creator."
+
+        creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+        creator.create("Individual", array.array, typecode='i', fitness=creator.FitnessMin)
+
+        return creator
+
+    def toolbox(self, ind_size, evaluator):
+        "Creating the toolbox."
+
+        toolbox = base.Toolbox()
+
+        # Attribute generator
+        toolbox.register("indices", random.sample, xrange(ind_size), ind_size)
+
+        # Structure initializers
+        toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
+        toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+        toolbox.register("mate", tools.cxPartialyMatched)
+        toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.05)
+        toolbox.register("select", tools.selTournament, tournsize=3)
+        toolbox.register("evaluate", evaluator)
+
+        return toolbox
+
+    def stats(self):
+        stats = tools.Statistics(lambda ind: ind.fitness.values)
+        stats.register("avg", tools.mean)
+        stats.register("std", tools.std)
+        stats.register("min", min)
+        stats.register("max", max)
+        return stats
+
+    def run(self, toolbox):
+        random.seed(169)
+
+        pop = toolbox.population(n=300)
+
+        hof = tools.HallOfFame(1)
+        stats = tools.Statistics(lambda ind: ind.fitness.values)
+        stats.register("avg", tools.mean)
+        stats.register("std", tools.std)
+        stats.register("min", min)
+        stats.register("max", max)
+
+        algorithms.eaSimple(pop, toolbox, 0.7, 0.2, 2000, stats=stats,
+                            halloffame=hof)
+
+        return pop, stats, hof
