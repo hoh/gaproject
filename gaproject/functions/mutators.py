8d6260b3b3a7daad4eb048650c023f61cc8a6aa6
Hugo Herter <okso@okso.local>
2012-12-06 14:16:50 +0100

    Made code compliant with PEP
    http://www.python.org/dev/peps/pep-0008/

--- gaproject/functions/mutators.py
+++ gaproject/functions/mutators.py
@@ -2,28 +2,30 @@
 '''
 Here should come the mutation functions.
 '''
+
 import random
 
 
-def insertionMutation(individual,indpb):
-    """
-    chose an individual randomly and insert it at a random position
-    """
-    if(random.random()>indpb):
-        indexRandomNode = random.randint(0, len(individual)-1)
-        insertionPoint = random.randint(0, len(individual)-1)
-        removedValue=individual.pop(indexRandomNode)  
-        individual.insert(insertionPoint,removedValue)
+def insertionMutation(individual, indpb):
+    '''
+    Chosing an node randomly and inserting it at a random position.
+    '''
+    if (random.random() > indpb):
+        indexRandomNode = random.randint(0, len(individual) - 1)
+        insertionPoint = random.randint(0, len(individual) - 1)
+        removedValue = individual.pop(indexRandomNode)
+        individual.insert(insertionPoint, removedValue)
     return individual
 
-def inversionMutation(individual,indpb):
-    """
-    chose a Subtour then insert it reversed at a different part of the individual
-    """
-    if(random.random()>indpb):
+
+def inversionMutation(individual, indpb):
+    '''
+    Chosing a Subtour then inserting it reversed at a different part of the individual.
+    '''
+    if(random.random() > indpb):
         #compute the beginning and end of the Subtour.
-        begSubtour = random.randint(0, len(individual)-1)
-        endSubtour = random.randint(begSubtour, len(individual)-1)
+        begSubtour = random.randint(0, len(individual) - 1)
+        endSubtour = random.randint(begSubtour, len(individual) - 1)
 
         #extract the Subtour and reverse it
         Subtour = individual[begSubtour:endSubtour]
@@ -33,20 +35,21 @@ def inversionMutation(individual,indpb):
         del individual[begSubtour:endSubtour]
 
         #compute where to insert it
-        insertionPos = random.randint(0, len(individual)-1)
+        insertionPos = random.randint(0, len(individual) - 1)
 
         #create the new individual by inserting the Subtour at the insertion position
         individual[:] = individual[0:insertionPos] + Subtour + individual[insertionPos:]
 
     return individual
 
+
 def simpleInversionMutation(individual, indpb):
-    """
-    chose a Subtour then reverse it at a different part of the individual
-    """
-    if(random.random()>indpb):
-        begSubtour = random.randint(0, len(individual)-1)
-        endSubtour = random.randint(begSubtour, len(individual)-1)
+    '''
+    Chosing a Subtour then reversing it at a different part of the individual.
+    '''
+    if(random.random() > indpb):
+        begSubtour = random.randint(0, len(individual) - 1)
+        endSubtour = random.randint(begSubtour, len(individual) - 1)
 
         #extract the Subtour
         Subtour = individual[begSubtour:endSubtour]
