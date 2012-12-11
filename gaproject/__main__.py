abd51bc85b50cbddd42c5c4035e8651cb551d1f3
Hugo Herter <dev@hugoherter.com>
2012-12-11 15:47:31 +0100

    Reverted commit

--- gaproject/__main__.py
+++ gaproject/__main__.py
@@ -4,38 +4,29 @@
 This is the main file of the project, used to setup and launch the computation.
 '''
 
-
-
-import sys
-sys.path.append('../')
 import pprint
-import numpy
 
 from gaproject.data import Box
 from gaproject.mydeap import MyDeap
 from gaproject.analysis import plot
-from gaproject.bench import bench
+import gaproject.shared as shared
 
-from gaproject.functions.evaluators import Evaluators
-from gaproject.functions.mutators import mutShuffleIndexes, \
-                                         insertionMutation, \
-                                         inversionMutation, \
-                                         simpleInversionMutation
-from functions.mutators import insertionMutation
+import gaproject.sets
 
 
-def run(data, operators, plot_result=False):
+def run(data, operators):
     '''Launches a run for the given dataset and genetic operators.
-    - plot : True if we want to plot the result.
     '''
     # Running the DEAP:
     mydeap = MyDeap()
     toolbox = mydeap.toolbox(len(data), operators)
-    pop, stats, hof = mydeap.run(toolbox, 500)
+    pop, stats, hof = mydeap.run(toolbox,
+                                 shared.settings.generations,
+                                 shared.settings.population)
 
     print 'Best so far:', operators['evaluate'](hof[0])
 
-    if plot_result:
+    if shared.settings.plot:
         # Plotting the best result so far:
         plot(hof[0], data)
 
@@ -47,35 +38,34 @@ def run(data, operators, plot_result=False):
             }
 
 
-def main(plot=False):
+def main():
     'Launches all runs.'
 
     box = Box('data/TSPBenchmark')
     data = box.get('belgiumtour.tsp')  # or xqf131.tsp
+    shared.distance_map = data.dist_matrix()
 
-    if plot:
+    if shared.settings.plot:
         data.plot()
 
-    distance_map = data.dist_matrix()
-    evaluator = Evaluators(distance_map).evalTSP
+    results = {}
 
-    results = []
-
-    for b in bench:
-        operators = bench[b]  # !! Must resolve names first
-        operators['evaluate'] = evaluator
+    sets = gaproject.sets.get()
+    for b in sets:
+        set_b = sets[b]
+        operators = gaproject.sets.evaluate(set_b)
 
         result = run(data, operators)
         results[b] = result
 
-                    # 'average': numpy.average(scores),
-                    # 'std': numpy.std(scores),
+        # 'average': numpy.average(scores),
+        # 'std': numpy.std(scores),
 
     # Pretty printing the resulting scores:
     pprint.pprint(results)
 
 if __name__ == '__main__':
     try:
-        hof = main(plot=('plot' in sys.argv))
+        hof = main()
     except KeyboardInterrupt:
         print 'Execution stopped.'
