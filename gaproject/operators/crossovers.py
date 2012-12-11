703d421312c5dde259aac3627042fd9f234ae0cd
aarribas <andres.arribas@gmail.com>
2012-12-11 18:33:29 +0100

    Worked in Crossovers

--- gaproject/operators/crossovers.py
+++ gaproject/operators/crossovers.py
@@ -1,38 +1,53 @@
 
 from deap import creator
 import random
-import shared
+import gaproject.shared as shared
+import numpy
 
 
-
-
-def cxSCX(individual1, individual2):
+def cxEnhancedSCX(individual1, individual2):
     """
-    Function that creates an instance of CXSCXCalculator and performs the crossover
+    Function that creates an instance of CXSCXCalculator and performs the simple SCX crossover
 
     """
-    return CXSCXCalculator(individual1, individual2, distanceMap).crossover()
+    medians = numpy.median(shared.distance_map, axis=1)
+    defaultSequence = range(1, len(individual1))
+    sequenceOfNodes = sorted(defaultSequence)
+    #creating the sequence of nodes like this is inneficient BUT it should work for the time being.
+    return CXSCXCalculator(individual1, individual2, sequenceOfNodes).crossover()
+
+
+def cxSimpleSCX(individual1, individual2):
+    """
+    Function that creates an instance of CXSCXCalculator and performs the simple SCX crossover
+
+    """
+    sequenceOfNodes = [x for x in range(2, len(individual1))]
+    return CXSCXCalculator(individual1, individual2, sequenceOfNodes).crossover()
+
 
 class CXSCXCalculator:
+
     """
     Contains the logic to perform sequential constructive crossover
     see: http://www.cscjournals.org/csc/manuscript/Journals/IJBB/volume3/Issue6/IJBB-41.pdf
 
     """
-    def __init__(self, individual1, individual2):
+
+    def __init__(self, individual1, individual2, sequenceOfNodes):
         self.individual2 = individual2
         self.individual1 = individual1
         self.visitedNodes = []
-        self.sequent
+        self.sequenceOfNodes = sequenceOfNodes
 
     def crossover(self):
         child = creator.Individual()
-        #start at one
-        currentNodePosIn1 = self.individual1.index(1)
-        currentNodePosIn2 = self.individual2.index(1)
+        #start with node number 1
+        currentNode = 1
         self.visitedNodes.append(1)
-
         while(True):
+            currentNodePosIn1 = self.individual1.index(currentNode)
+            currentNodePosIn2 = self.individual2.index(currentNode)
             candidateLegitNodeIn1 = self.individual1[(currentNodePosIn1 + 1) % len(self.individual1)]
             candidateLegitNodeIn2 = self.individual2[(currentNodePosIn2 + 1) % len(self.individual2)]
             #check if the candidate node in individual 1 has not been visited
@@ -40,7 +55,7 @@ class CXSCXCalculator:
                 legitNodeIn1 = candidateLegitNodeIn1
             else:
                 legitNodeIn1 = None
-            #check if the candidate node in individual 2 has not been visited                
+            #check if the candidate node in individual 2 has not been visited 
             if candidateLegitNodeIn2 not in self.visitedNodes:
                 legitNodeIn2 = candidateLegitNodeIn2
             else:
@@ -48,17 +63,33 @@ class CXSCXCalculator:
             #if both individuals contain legit nodes, chose the one forming the edge
             #with lowest cost
             if(legitNodeIn1 and legitNodeIn2):
-                if shared.distanceMap[currentNodePosIn1][candidateLegitNodeIn1] > shared.distanceMap[currentNodePosIn2][candidateLegitNodeIn2]:
-                    child.append(self.individual2[currentNodePosIn2])
+                if shared.distance_map[currentNode][legitNodeIn1] > shared.distance_map[currentNode][legitNodeIn2]:
+                    child.append(legitNodeIn2)
+                    self.visitedNodes.append(legitNodeIn2)
+                    currentNode = legitNodeIn2
+                    if len(child) == len(self.individual1):
+                        break
                 else:
-                    child.append(self.individual1[currentNodePosIn1])
+                    child.append(legitNodeIn1)
+                    self.visitedNodes.append(legitNodeIn1)
+                    currentNode = legitNodeIn1
+                    if len(child) == len(self.individual1):
+                        break
             else:
-                child.append(self._getSequentialNode())
+                sequentialNode = self._getSequentialNode()
+                child.append(sequentialNode)
+                self.visitedNodes.append(sequentialNode)
+                currentNode = sequentialNode
+                if len(child) == len(self.individual1):
+                        break
 
         return child
 
     def _getSequentialNode(self):
-        return 1
+        for node in self.sequenceOfNodes:
+            if node not in self.visitedNodes:
+                self.visitedNodes.append(node)
+                return node
 
 
 def cxERX(individual1, individual2):
