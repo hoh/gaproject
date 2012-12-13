27b0883be184a67d0e1c01808bea88dbcb67cd32
Hugo Herter <dev@hugoherter.com>
2012-12-13 11:36:24 +0100

    Added fitness_plot(stats), Fixed naming error

--- gaproject/analysis.py
+++ gaproject/analysis.py
@@ -1,7 +1,6 @@
 
 "Module to analyze the result we've got. Eg: plot it."
 
-import pprint
 import gaproject.shared as shared
 settings = shared.settings
 
@@ -19,18 +18,27 @@ def plot(individual, data):
     plt.show()
 
 
+def fitness_plot(stats):
+    import matplotlib.pyplot as plt
+
+    data_min = [fit[0] for fit in stats.data['min'][0]]
+    # import pdb; pdb.set_trace()
+    plt.plot(data_min)
+    plt.show()
+
+
 def analyse(results):
     import numpy
 
     for run in results:
         values = results[run]
-        operators = values['operators']
+        set_ = values['set']
         print
         print 'Run:', run
         print 'Fitness average:', numpy.average(values['fitness'])
         print 'Fitness std:', numpy.std(values['fitness'])
-        print 'Generations:', settings.fallback(operators, 'generations')
-        print 'Population:', settings.fallback(operators, 'population')
+        print 'Generations:', settings.fallback(set_, 'generations')
+        print 'Population:', settings.fallback(set_, 'population')
         print 'n =', len(values['fitness'])
         print
         # pprint.pprint(values)
