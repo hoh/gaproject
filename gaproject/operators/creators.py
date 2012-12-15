9c34c71fc175cd1b48e8a5b64766ae33005c14cc
aarribas <andres.arribas@gmail.com>
2012-12-15 17:32:46 +0100

    Adj creator has been created!

--- gaproject/operators/creators.py
+++ gaproject/operators/creators.py
@@ -1,9 +1,14 @@
 
 import random
 import gaproject.shared as shared
-
+from gaproject.operators.evaluators import fromPathToAdjacent
 
 def path_creator():
     'Creates a path individual'
     ind_size = len(shared.data)
     return random.sample(xrange(ind_size), ind_size)
+
+def adj_creator():
+    'Creates a valid adjacent individual'
+    ind_size = len(shared.data)
+    return fromPathToAdjacent(random.sample(xrange(ind_size), ind_size))
