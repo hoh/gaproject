471b2ea674e94f3d946eb6f230a5f15b2d9a6fb8
aarribas <andres.arribas@gmail.com>
2012-12-17 21:54:34 +0100

    Detached the launcher part of the optimizer to be able to use it for the adj vs path representation comparison and the benchmarking This introduces a new Launcher class with methods -clear -load_data -launch_queue

--- gaproject/tools/optimizer.py
+++ gaproject/tools/optimizer.py
@@ -1,13 +1,12 @@
-
-from numpy import average
-
-import gaproject.run
 from gaproject.data import Box
-from gaproject.tools.results import Results, RunTable
+from gaproject.tools.launcher import Launcher
 
 
 class Optimizer(object):
     'Main class for parameter optimization.'
+
+    def __init__(self):
+        self.launcher = Launcher()
 
     def go(self):
         '''
@@ -15,21 +14,19 @@ class Optimizer(object):
 
         1. Clears queue and results.
         2. Queues new results for variable range of interest
-        3. Executing the queue
-        4. Getting best result to the queue
-        5. New variable and back to [2].
+        3. Executing the queue and retrieving the best result
+        4. New variable and back to [2].
 
         '''
         print 'Go go go !'
 
         # 0. Loading data
         box = Box('data/TSPBenchmark')
-        self.data = box.get('xqf131.tsp')
+        data = box.get('xqf131.tsp')
+        self.launcher.load_data(data)
 
-        # 1.
-        self.store = gaproject.store.Store()
-        self.store.runs.remove({})
-        self.store.jobs.remove({})
+        # 1. clear queue and results
+        self.launcher.clear()
 
         # 2. Create dictionnaries for range
         base = {
@@ -52,7 +49,7 @@ class Optimizer(object):
             job['generations'] = 50000 / pop
             jobs.append(job)
 
-        best = self.launch_queue(jobs, plot_all=False)['set']
+        best = self.launcher.launch_queue(jobs, plot_all=False)['set']
         del best['_id']
 
         print 'Optimizing cxpb'
@@ -63,7 +60,7 @@ class Optimizer(object):
             job['cxpb'] = cxpb / 100.
             jobs.append(job)
 
-        best = self.launch_queue(jobs, plot_all=False)['set']
+        best = self.launcher.launch_queue(jobs, plot_all=False)['set']
         del best['_id']
 
         print 'Optimizing indpb'
@@ -74,7 +71,7 @@ class Optimizer(object):
             job['mutate'] = (job['mutate'][0], {'indpb': mut / 100.})
             jobs.append(job)
 
-        best = self.launch_queue(jobs, plot_all=False)['set']
+        best = self.launcher.launch_queue(jobs, plot_all=False)['set']
         del best['_id']
 
         print 'Optimizing mutpb'
@@ -85,7 +82,7 @@ class Optimizer(object):
             job['mutpb'] = mutpb / 100.
             jobs.append(job)
 
-        best = self.launch_queue(jobs, plot_all=False)['set']
+        best = self.launcher.launch_queue(jobs, plot_all=False)['set']
         del best['_id']
 
         print 'Optimizing crossover operator'
@@ -106,7 +103,7 @@ class Optimizer(object):
 
             jobs.append(job)
 
-        best = self.launch_queue(jobs, plot_all=True)['set']
+        best = self.launcher.launch_queue(jobs, plot_all=True)['set']
         del best['_id']
 
         print 'Optimizing mutation operator'
@@ -123,41 +120,9 @@ class Optimizer(object):
 
             jobs.append(job)
 
-        best = self.launch_queue(jobs, plot_all=True)['set']
+        best = self.launcher.launch_queue(jobs, plot_all=True)['set']
         del best['_id']
 
         print 'runned'
         raw_input()
         # Getting best
-
-    def launch_queue(self, jobs, plot_all):
-        'Launches a queue of jobs and returns the best result.'
-        self.store.runs.remove({})
-        self.store.jobs.remove({})
-
-        for job in jobs:
-            self.store.jobs.insert(job)
-
-        # 3.
-        gaproject.run.main(self.data)
-
-        # 4.
-        results = Results()
-        results.print_()
-        if plot_all:
-            results.plotDifferentRuns()
-
-        best = results.find_one()
-        best_avg = average(best['fitness'])
-        for run in results.find():
-            run_avg = average(run['fitness'])
-            if run_avg < best_avg:
-                best = run
-                best_avg = run_avg
-
-        print 'Best run so far:'
-        table = RunTable()
-        table.add_run(best)
-        print table
-
-        return best
